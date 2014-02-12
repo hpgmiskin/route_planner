2616cea1773f8d23d601111141b6ce788cbcaf9d
miskinh <henry.miskin@talktalk.net>
2014-02-12 20:21:53 +0000

    Improved airFoil to return CD and CL

--- airFoil.py
+++ airFoil.py
@@ -2,50 +2,71 @@
 
 import re,urllib.request
 
-def getFoilData(foilName=None,reynoldsNumber=None):
-	"returns a dictionary of the airfoils lift and drag coeficients at differnt angles of attack"
+class AirFoil():
+	"""class to load airfoil data and return the lift and drag values depending on the angle of attack"""
 
-	#load list of foil names
-	with open("airfoils.txt") as openFile:
-		airfoils = openFile.read().splitlines()
+	def __init__(self, foilName="naca23015-il", reynoldsNumber=50000):
+		"initiates the class by calling setFoilData that populates alpha, CL and CD"
 
-	#check requested foil is in list
-	if (foilName not in airfoils):
-		raise ValueError("Foil name {} does not exist".format(foilName))
+		self.AoAs = []
+		self.CDs = []
+		self.CLs = []
+		
+		self.setFoilData(foilName,reynoldsNumber)
 
-	#check requested reynoldy number exists
-	reynoldsNumbers = [50000,100000,200000,500000,1000000]
-	if (reynoldsNumber not in reynoldsNumbers):
-		raise ValueError("Reynoldy number {} does not exist".format(reynoldsNumber))
+	def getCL(self,alpha):
+		"returns the closes approximation of the coeficient of lift given the angle of attack alpha"
 
-	#access the data file
-	url = 'http://airfoiltools.com/polar/text?polar=xf-{}-{}'.format(foilName,reynoldsNumber)
-	with urllib.request.urlopen(url) as webPage:
-		content = webPage.read().splitlines()
+		difference = [abs(AoA-alpha) for AoA in self.AoAs]
+		minimumIndex = difference.index(min(difference))
 
-	#set not to record data until certain point
-	record = False
-	#construct dictionary to hold cvalues
-	result = {"alpha":[],"CL":[],"CD":[]}
+		return self.CLs[minimumIndex]
 
-	#cycle through lines of content to obtain alpha, CD and CL
-	for row in content:
-		row = str(row)
-		rowList = re.split(r'\s*',row)
+	def getCD(self,alpha):
+		"returns the closes approximation of the coeficient of drag given the angle of attack alpha"
 
-		if (record and ("--" not in row)):
-			result["alpha"].append(float(rowList[1]))
-			result["CL"].append(float(rowList[2]))
-			result["CD"].append(float(rowList[3]))
+		difference = [abs(AoA-alpha) for AoA in self.AoAs]
+		minimumIndex = difference.index(min(difference))
 
-		if (("alpha" == rowList[1]) and ("CL" == rowList[2]) and ("CD" == rowList[3])):
-			record = True
+		return self.CDs[minimumIndex]
 
-	return result
+	def setFoilData(self,foilName,reynoldsNumber):
+		"returns a dictionary of the airfoils lift and drag coeficients at differnt angles of attack"
+
+		#load list of foil names
+		with open("airfoils.txt") as openFile:
+			airfoils = openFile.read().splitlines()
+
+		#check requested foil is in list
+		if (foilName not in airfoils):
+			raise ValueError("Foil name {} does not exist".format(foilName))
+
+		#check requested reynoldy number exists
+		reynoldsNumbers = [50000,100000,200000,500000,1000000]
+		if (reynoldsNumber not in reynoldsNumbers):
+			raise ValueError("Reynoldy number {} does not exist".format(reynoldsNumber))
+
+		#access the data file
+		url = 'http://airfoiltools.com/polar/text?polar=xf-{}-{}'.format(foilName,reynoldsNumber)
+		with urllib.request.urlopen(url) as webPage:
+			content = webPage.read().splitlines()
+
+		#set not to record data until certain point
+		record = False
+
+		#cycle through lines of content to obtain alpha, CD and CL
+		for row in content:
+			row = str(row)
+			rowList = re.split(r'\s*',row)
+
+			if (record and ("--" not in row)):
+				self.AoAs.append(float(rowList[1]))
+				self.CLs.append(float(rowList[2]))
+				self.CDs.append(float(rowList[3]))
+
+			if (("alpha" == rowList[1]) and ("CL" == rowList[2]) and ("CD" == rowList[3])):
+				record = True
 
 if __name__ == "__main__":
-	result = getFoilData("ag45c03-il",50000)
-
-	print(result["alpha"])
-	print(result["CD"])
-	print(result["CL"])
+	airFoil = AirFoil("naca23015-il",100000)
+	print(airFoil.getCD(8.25))
